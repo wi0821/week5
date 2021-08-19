98bc77c7fead963db41d81aa9ca9c227e525b8f7
WilsonHan <Win05118@gmail.com>
2021-08-20 05:21:55 +0800

    Update 2021-08-19T21:21:51.263Z

---
+++ assets/js/all.js
@@ -0,0 +1,21 @@
+"use strict";
+
+$(document).ready(function () {
+  $('#bottom-Reply-Btn').click(function (e) {
+    e.preventDefault();
+    $('#assignment-Comment').toggleClass('d-none');
+    $('#assignment-Bottom').toggleClass('d-none');
+    $('#assignment-Reply').removeClass('d-none');
+  });
+  $('#assignment-Reply-Btn,#assignment-Cancel-Btn').click(function (e) {
+    e.preventDefault();
+    $('#assignment-Comment').removeClass('d-none');
+    $('#assignment-Bottom').removeClass('d-none');
+    $('#assignment-Reply').toggleClass('d-none');
+  });
+  $('.iconExpand').click(function (e) {
+    e.preventDefault();
+    $('.iconExpand').toggleClass('active');
+  });
+});
+//# sourceMappingURL=all.js.map
